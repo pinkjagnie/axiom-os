718488836413371bf55e8b73818650cc32214615
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-06-22 18:49:29 +0200

    Updated release date&banner, added links to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { GiPawHeart } from "react-icons/gi";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter, faGithub, faTelegram, faPaypal, faPatreon } from "@fortawesome/free-brands-svg-icons";
 
@@ -12,22 +13,23 @@ const Footer = () => {
         <div className={classes.copyright}>
           <p>&copy; AxiomOS project</p>
           <p>Want to contact with us? <a href="mailto:contact@axiomos.pl" className={classes.mail}>Just send us an email</a></p>
-          <p>Built with <span className={classes.author} role="img" aria-label="Love">&#9829;</span> by <span className={classes.author}>PinkLlama</span></p>
+          <p>Built with <GiPawHeart className={classes.author} /> by <span><a href="https://github.com/pinkjagnie" target="_blank" rel="noopener noreferrer" className={classes.author}>PinkJagnie</a></span></p>
+          
         </div>
         <div className={classes.socialContainer}>
-          <a href="">
+          <a href="https://twitter.com/axiomos_aosp" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faTwitter} className={classes.social} />
           </a>
-          <a href="">
+          <a href="" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} className={classes.social} />
           </a>
-          <a href="">
+          <a href="https://t.me/axiomoshub" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faTelegram} className={classes.social} />
           </a>
-          <a href="">
+          <a href="" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faPaypal} className={classes.social} />
           </a>
-          <a href="">
+          <a href="" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faPatreon} className={classes.social} />
           </a>
         </div>
